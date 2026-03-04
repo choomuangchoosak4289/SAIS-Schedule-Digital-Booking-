2ecaa242d9fd1b6456325676b0512cfa10e729ef
choomuangchoosak4289 <choomuangchoosak4289@gmail.com>
2026-03-05 01:23:14 +0700

    Add configuration settings in config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+window.SAIS_CONFIG = {
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyr7qdIlQ-wNPbor0u1VxvQVIfRSgN0-l4yR-_rYGO9fN9_TCDw3uAe1oQLlCYgpqQD/exec",
+    ADMIN_USERNAME: "jiraphong2227"
+};

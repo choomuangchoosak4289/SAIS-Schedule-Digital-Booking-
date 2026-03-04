7bcbcc4b4f8ce201f3aee01538c498500821a086
choomuangchoosak4289 <choomuangchoosak4289@gmail.com>
2026-03-05 01:43:42 +0700

    Remove comment from config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// config.js
+
 window.SAIS_CONFIG = {
     SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyr7qdIlQ-wNPbor0u1VxvQVIfRSgN0-l4yR-_rYGO9fN9_TCDw3uAe1oQLlCYgpqQD/exec",
     ADMIN_USERNAME: "jiraphong2227"

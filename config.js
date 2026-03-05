9cd718bbd97b925447ae3b1a3066feb9f53c8111
choomuangchoosak4289 <choomuangchoosak4289@gmail.com>
2026-03-05 17:23:36 +0700

    Change SCRIPT_URL in config.js
    Updated SCRIPT_URL in SAIS_CONFIG to a new endpoint.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-
+// config.js
 window.SAIS_CONFIG = {
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyr7qdIlQ-wNPbor0u1VxvQVIfRSgN0-l4yR-_rYGO9fN9_TCDw3uAe1oQLlCYgpqQD/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyBIuHb9shBoAFV7WVp_KnlvNTRI4MuiIMfg8Iq8DA8sSrE37hR3L3SFBhDZWR1rCUZ/exec",
     ADMIN_USERNAME: "jiraphong2227"
 };
